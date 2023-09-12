fc71f000d16668a1c0064b41e1197931e5ba5df9
Vince <97965361+VinceSMC@users.noreply.github.com>
2023-09-12 12:58:48 +0200

    Add files via upload

---
+++ smc-developments_page-loader_script.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', function() {
+    const loader = document.querySelector('.page-loader');
+    const contentInsideWrapper = document.querySelectorAll('.liquid_page-wrapper > *');
+
+    // Make the content inside .liquid_page-wrapper visible
+    contentInsideWrapper.forEach(item => {
+        item.style.visibility = 'visible';
+    });
+
+    if (loader) {
+        // Fade out the loader
+        loader.style.opacity = '0';
+
+        // After the transition, set display to none for the loader
+        setTimeout(() => {
+            loader.style.display = 'none';
+        }, 300); // Matches the transition time in the CSS
+    }
+});
