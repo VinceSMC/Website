bdbffb134833461c992d75e5211a9229f176d36b
Vince <97965361+VinceSMC@users.noreply.github.com>
2023-09-15 15:00:41 +0200

    Add files via upload

---
+++ smc-developments_share-update_script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Function to show the modal and change the text to the link
+    document.querySelector('.changelog__social-share').addEventListener('click', function(event) {
+        
+        const overlay = document.querySelector('.changelog__modal-overlay');
+        overlay.style.display = 'flex';
+        setTimeout(() => overlay.classList.add('show-modal'), 10);
+        
+        // Get the dynamic link from the closest parent .changelog__list's .modal__shareable-link href attribute
+        const dynamicLink = this.closest('.changelog__list').querySelector('.modal__shareable-link').href;
+
+        // Change the text inside the modal to the link
+        const linkText = document.querySelector('.modal__link-text');
+        linkText.textContent = dynamicLink;
+    });
+
+    // Function to hide the modal when overlay or close SVG is clicked
+    document.querySelector('.changelog__modal-overlay').addEventListener('click', function(e) {
+        if (e.target === this || e.target.closest('.modal__close')) {
+            const overlay = document.querySelector('.changelog__modal-overlay');
+            overlay.classList.remove('show-modal');
+            setTimeout(() => {
+                overlay.style.display = 'none';
+            }, 300);
+        }
+    });
+
+    // Code to copy the CMS link
+    const copyButton = document.querySelector('.modal__copy-button');
+    copyButton.addEventListener('click', function() {
+        const link = document.querySelector('.modal__link-text').textContent;
+        const tempInput = document.createElement('input');
+        document.body.appendChild(tempInput);
+        tempInput.value = link;
+        tempInput.select();
+        document.execCommand('copy');
+        document.body.removeChild(tempInput);
+
+        // Change button text to "Copied!" and revert after 1 second
+        const originalText = copyButton.textContent;
+        copyButton.textContent = 'Copied!';
+        setTimeout(() => {
+            copyButton.textContent = originalText;
+        }, 1000);
+    });
+});
