0ddeccdb6877ae2f8bd9c1dc3946266f9445a8b4
Vince <97965361+VinceSMC@users.noreply.github.com>
2023-09-15 12:39:27 +0200

    Update smc-developments_changelog_script.js

--- smc-developments_changelog_script.js
+++ smc-developments_changelog_script.js
@@ -1,41 +1,38 @@
-let changeLogSvg = $('[data-name="changelog-icon"]')
-
+let changeLogSvg = $('[data-name="changelog-icon"]');
 
 function clean_parse_html(){
     $('.w-richtext').children('p').each(function() {                
         if(this.innerHTML.indexOf('&lt;') == 0 && this.innerHTML.match(/&gt;$/) != null) {
-          this.innerHTML = this.innerText.replace(/\u00A0/g, '');
-					
+            this.innerHTML = this.innerText.replace(/\u00A0/g, '');
         }      	
-				if(!($(this).children().length > 0)){
-					$(this).remove()
-				}
-    });
-		
-}
-function insertSVG(){
-		$('.changelog__item').each(function(){
-				if(!($(this).children('[data-name="changelog-icon"]').length) ){
-            $(this).prepend(changeLogSvg.clone())
+        if(!($(this).children().length > 0)){
+            $(this).remove();
         }
-		})
-		
-		
+    });		
 }
 
-clean_parse_html()
-insertSVG()
+function insertSVG(){
+    $('.changelog__item').each(function(){
+        if(!($(this).children('[data-name="changelog-icon"]').length) ){
+            let clonedSvg = changeLogSvg.clone();
+            clonedSvg.addClass('is--dark-mode');
+            $(this).prepend(clonedSvg);
+        }
+    });
+}
+
+clean_parse_html();
+insertSVG();
 
 window.fsAttributes = window.fsAttributes || [];
 window.fsAttributes.push([
-  'cmsload',
-  (listInstances) => {
-    
-    const [listInstance] = listInstances;
+    'cmsload',
+    (listInstances) => {
+        const [listInstance] = listInstances;
 
-    listInstance.on('renderitems', (renderedItems) => {
-     	clean_parse_html()
-      insertSVG()
-    });
-  },
+        listInstance.on('renderitems', (renderedItems) => {
+            clean_parse_html();
+            insertSVG();
+        });
+    },
 ]);
