c408cd2e56b6269ae08aaa247796b2baa163023f
Vince <97965361+VinceSMC@users.noreply.github.com>
2023-10-07 18:15:36 +0200

    Update smc-developments_updates_time-ago-format_script.js

--- smc-developments_updates_time-ago-format_script.js
+++ smc-developments_updates_time-ago-format_script.js
@@ -16,24 +16,25 @@ document.addEventListener("DOMContentLoaded", function() {
         let minute = 60 * 1000;
         let hour = minute * 60;
         let day = hour * 24;
-        let week = day * 7;
+        let month = day * 30;
+        let year = day * 365;
 
         let result = '';
         if (difference < hour) {
             let minutesPassed = Math.floor(difference / minute);
-            result = minutesPassed + ' minutes ago';
+            result = minutesPassed === 1 ? '1 minute ago' : minutesPassed + ' minutes ago';
         } else if (difference < day) {
             let hoursPassed = Math.floor(difference / hour);
-            result = hoursPassed + ' hours ago';
-        } else if (difference < (2 * day)) {
-            let options = { hour: 'numeric', minute: 'numeric' };
-            result = 'Yesterday at ' + dateTime.toLocaleTimeString(undefined, options);
-        } else if (difference < week) {
-            let options = { weekday: 'long', hour: 'numeric', minute: 'numeric' };
-            result = dateTime.toLocaleDateString(undefined, options);
+            result = hoursPassed === 1 ? '1 hour ago' : hoursPassed + ' hours ago';
+        } else if (difference < month) {
+            let daysPassed = Math.floor(difference / day);
+            result = daysPassed === 1 ? '1 day ago' : daysPassed + ' days ago';
+        } else if (difference < year) {
+            let monthsPassed = Math.floor(difference / month);
+            result = monthsPassed === 1 ? '1 month ago' : monthsPassed + ' months ago';
         } else {
-            let options = { month: 'long', day: 'numeric', year: 'numeric' };
-            result = dateTime.toLocaleDateString(undefined, options);
+            let yearsPassed = Math.floor(difference / year);
+            result = yearsPassed === 1 ? '1 year ago' : yearsPassed + ' years ago';
         }
         
         element.textContent = result;
