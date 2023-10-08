d522b12850398b2a75badbbe9324ce91c71abe12
Vince <97965361+VinceSMC@users.noreply.github.com>
2023-10-08 11:07:11 +0200

    Update smc-developments_updates_time-ago-format_script.js

--- smc-developments_updates_time-ago-format_script.js
+++ smc-developments_updates_time-ago-format_script.js
@@ -1,46 +1,53 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('.changelog__date-time-ago').forEach(function(element) {
-        // Find the parent CMS item and the hidden date field
-        let cmsItem = element.closest('.cms-item');
-        let dateElement = cmsItem ? cmsItem.querySelector('.changelog__hidden-date') : null;
+    // Get the element by ID 'changelogDateTimeAgo'
+    let element = document.getElementById('changelogDateTimeAgo');
 
-        if (!dateElement) {
-            console.error('No date element found for: ', element);
-            return;
-        }
+    // Ensure the element is found
+    if (!element) {
+        console.error('Element with ID changelogDateTimeAgo not found');
+        return;
+    }
 
-        let dateTime = new Date(dateElement.textContent);
-        let now = new Date();
-        let difference = now - dateTime;
-        
-        let minute = 60 * 1000;
-        let hour = minute * 60;
-        let day = hour * 24;
-        let week = day * 7;
-        let month = day * 30;
-        let year = day * 365;
+    // Assuming the ID for the 'cms-item' class is 'cmsItem' and the ID for the 'changelog__hidden-date' class is 'changelogHiddenDate'
+    let cmsItem = element.closest('#cmsItem');
+    let dateElement = cmsItem ? cmsItem.querySelector('#changelogHiddenDate') : null;
 
-        let result = '';
-        if (difference < hour) {
-            let minutesPassed = Math.floor(difference / minute);
-            result = minutesPassed === 1 ? '1 minute ago' : minutesPassed + ' minutes ago';
-        } else if (difference < day) {
-            let hoursPassed = Math.floor(difference / hour);
-            result = hoursPassed === 1 ? '1 hour ago' : hoursPassed + ' hours ago';
-        } else if (difference < week) {
-            let daysPassed = Math.floor(difference / day);
-            result = daysPassed === 1 ? '1 day ago' : daysPassed + ' days ago';
-        } else if (difference < (4 * week)) { // Less than 4 weeks
-            let weeksPassed = Math.floor(difference / week);
-            result = weeksPassed === 1 ? '1 week ago' : weeksPassed + ' weeks ago';
-        } else if (difference < year) {
-            let monthsPassed = Math.floor(difference / month);
-            result = monthsPassed === 1 ? '1 month ago' : monthsPassed + ' months ago';
-        } else {
-            let yearsPassed = Math.floor(difference / year);
-            result = yearsPassed === 1 ? '1 year ago' : yearsPassed + ' years ago';
-        }
-        
-        element.textContent = result;
-    });
+    if (!dateElement) {
+        console.error('No date element found for: ', element);
+        return;
+    }
+
+    let dateTime = new Date(dateElement.textContent);
+    let now = new Date();
+    let difference = now - dateTime;
+    
+    let minute = 60 * 1000;
+    let hour = minute * 60;
+    let day = hour * 24;
+    let week = day * 7;
+    let month = day * 30;
+    let year = day * 365;
+
+    let result = '';
+    if (difference < hour) {
+        let minutesPassed = Math.floor(difference / minute);
+        result = minutesPassed === 1 ? '1 minute ago' : minutesPassed + ' minutes ago';
+    } else if (difference < day) {
+        let hoursPassed = Math.floor(difference / hour);
+        result = hoursPassed === 1 ? '1 hour ago' : hoursPassed + ' hours ago';
+    } else if (difference < week) {
+        let daysPassed = Math.floor(difference / day);
+        result = daysPassed === 1 ? '1 day ago' : daysPassed + ' days ago';
+    } else if (difference < (4 * week)) { 
+        let weeksPassed = Math.floor(difference / week);
+        result = weeksPassed === 1 ? '1 week ago' : weeksPassed + ' weeks ago';
+    } else if (difference < year) {
+        let monthsPassed = Math.floor(difference / month);
+        result = monthsPassed === 1 ? '1 month ago' : monthsPassed + ' months ago';
+    } else {
+        let yearsPassed = Math.floor(difference / year);
+        result = yearsPassed === 1 ? '1 year ago' : yearsPassed + ' years ago';
+    }
+    
+    element.textContent = result;
 });
