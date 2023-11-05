9d0618a84b4875c9d780158e153914c1ebd18849
Vince <97965361+VinceSMC@users.noreply.github.com>
2023-11-05 16:10:22 +0100

    Update smc-developments_share-update_script.js

--- smc-developments_share-update_script.js
+++ smc-developments_share-update_script.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    // Attach click event to all elements with the "data-link-share" attribute
-    const shareButtons = document.querySelectorAll('[data-link-share]');
+    // Attach click event to all elements with the "data-changelog-social-share" attribute
+    const shareButtons = document.querySelectorAll('[data-changelog-social-share]');
     shareButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function(event) {
+            event.preventDefault(); // prevent default action if it's an anchor or a button inside a form
             const overlay = document.querySelector('.changelog__modal-overlay');
             overlay.style.display = 'flex';
             setTimeout(() => overlay.classList.add('show-modal'), 10);
 
-            // Get the dynamic link from the closest parent with the "data-link-share" attribute
+            // Get the dynamic link from the element with "data-modal-shareable-link" attribute within the closest parent with "data-link-share"
             const dynamicLink = this.closest('[data-link-share]').querySelector('[data-modal-shareable-link]').href;
 
             // Change the text inside the modal to the link
