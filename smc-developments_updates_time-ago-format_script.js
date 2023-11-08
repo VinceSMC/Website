b0cd99f7c0c71d1fe9a38314b1b3360c39c2de44
Vince <97965361+VinceSMC@users.noreply.github.com>
2023-11-08 10:39:23 +0100

    Update smc-developments_updates_time-ago-format_script.js

--- smc-developments_updates_time-ago-format_script.js
+++ smc-developments_updates_time-ago-format_script.js
@@ -12,33 +12,42 @@ document.addEventListener("DOMContentLoaded", function() {
         let dateTime = new Date(dateElement.textContent);
         let now = new Date();
         let difference = now - dateTime;
-        
+
         let minute = 60 * 1000;
         let hour = minute * 60;
         let day = hour * 24;
-        let week = day * 7;
-        let month = day * 30;
-        let year = day * 365;
 
         let result = '';
-        if (difference < hour) {
-            let minutesPassed = Math.floor(difference / minute);
-            result = minutesPassed === 1 ? '1 minute ago' : minutesPassed + ' minutes ago';
+        if (difference < minute) {
+            result = 'Just now';
+        } else if (difference < hour) {
+            let minutesPassed = Math.round(difference / minute);
+            result = minutesPassed + ' minutes ago';
         } else if (difference < day) {
-            let hoursPassed = Math.floor(difference / hour);
-            result = hoursPassed === 1 ? '1 hour ago' : hoursPassed + ' hours ago';
-        } else if (difference < week) {
+            let hoursPassed = Math.round(difference / hour);
+            result = hoursPassed + ' hours ago';
+        } else if (difference < (day * 30)) {
             let daysPassed = Math.floor(difference / day);
-            result = daysPassed === 1 ? '1 day ago' : daysPassed + ' days ago';
-        } else if (difference < (4 * week)) { 
-            let weeksPassed = Math.floor(difference / week);
-            result = weeksPassed === 1 ? '1 week ago' : weeksPassed + ' weeks ago';
-        } else if (difference < year) {
-            let monthsPassed = Math.floor(difference / month);
-            result = monthsPassed === 1 ? '1 month ago' : monthsPassed + ' months ago';
+            result = daysPassed + ' days ago';
         } else {
-            let yearsPassed = Math.floor(difference / year);
-            result = yearsPassed === 1 ? '1 year ago' : yearsPassed + ' years ago';
+            // Calculate the month difference
+            let yearDiff = now.getFullYear() - dateTime.getFullYear();
+            let monthDiff = now.getMonth() - dateTime.getMonth() + (yearDiff * 12);
+            let dayDiff = now.getDate() - dateTime.getDate();
+            
+            if (dayDiff < 0) {
+                // If the day part of the 'now' date is less than the day part of the 'dateTime', 
+                // it means we haven't completed a month.
+                monthDiff -= 1;
+            }
+
+            // If monthDiff is 0, it means we're in the same month and should show the day difference.
+            if (monthDiff <= 0) {
+                let daysPassed = Math.floor(difference / day);
+                result = daysPassed + ' days ago';
+            } else {
+                result = monthDiff === 1 ? '1 month ago' : monthDiff + ' months ago';
+            }
         }
         
         element.textContent = result;
