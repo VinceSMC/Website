9fa356b4fc69b2729aada4f2fc1e8f5254ae4289
Vince <97965361+VinceSMC@users.noreply.github.com>
2024-01-27 21:14:58 +0100

    Update smc-developments_tooltip_script.js

--- smc-developments_tooltip_script.js
+++ smc-developments_tooltip_script.js
@@ -1,36 +1,40 @@
-tippy('#tooltip-top', {             // Use class or id
-  animation: 'scale',         // See docs for more options (there is a lot :) )
-  duration: 150,              // Duration for ToolTip Animation
-  arrow: true,                // Add ToolTip Arrow
-  delay: [0, 50],             // First # = delay in, second # = delay out
-  arrowType: 'sharp',         // Sharp or 'round' or remove for none
-  theme: 'light',             // Dark is the default
-  boundary: 'scrollParent',
-  maxWidth: 220,
-  interactive: false,
-})
+(function() {
+    document.addEventListener('DOMContentLoaded', function() {
+        tippy('#tooltip-top', {
+            animation: 'scale',
+            duration: 150,
+            arrow: true,
+            delay: [0, 50],
+            arrowType: 'sharp',
+            theme: 'light',
+            boundary: 'scrollParent',
+            maxWidth: 220,
+            interactive: false,
+        });
 
-tippy('#tooltip-top-interactive', {             // Use class or id
-  animation: 'scale',         // See docs for more options (there is a lot :) )
-  duration: 150,              // Duration for ToolTip Animation
-  arrow: true,                // Add ToolTip Arrow
-  delay: [0, 50],             // First # = delay in, second # = delay out
-  arrowType: 'sharp',         // Sharp or 'round' or remove for none
-  theme: 'light',             // Dark is the default
-  boundary: 'scrollParent',
-  maxWidth: 220,
-  interactive: true,
-})
+        tippy('#tooltip-top-interactive', {
+            animation: 'scale',
+            duration: 150,
+            arrow: true,
+            delay: [0, 50],
+            arrowType: 'sharp',
+            theme: 'light',
+            boundary: 'scrollParent',
+            maxWidth: 220,
+            interactive: true,
+        });
 
-tippy('.tippy-back', {       // Use class or id
-  animation: 'scale',         // See docs for more options (there is a lot :) )
-  placement: 'right',
-  duration: 150,              // Duration for ToolTip Animation
-  arrow: true,                // Add ToolTip Arrow
-  delay: [0, 50],             // First # = delay in, second # = delay out
-  arrowType: 'sharp',         // Sharp or 'round' or remove for none
-  theme: 'light',             // Dark is the default
-  boundary: 'scrollParent',
-  maxWidth: 220,
-  offset: "0, 10",
-})
+        tippy('.tippy-back', {
+            animation: 'scale',
+            placement: 'right',
+            duration: 150,
+            arrow: true,
+            delay: [0, 50],
+            arrowType: 'sharp',
+            theme: 'light',
+            boundary: 'scrollParent',
+            maxWidth: 220,
+            offset: "0, 10",
+        });
+    });
+})();
