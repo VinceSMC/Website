f3b868a34fcd9c750013f3a309ccba2f11dd8047
Vince <97965361+VinceSMC@users.noreply.github.com>
2024-08-21 18:47:07 +0200

    Update smc-developments_dark-light-mode_script.js

--- smc-developments_dark-light-mode_script.js
+++ smc-developments_dark-light-mode_script.js
@@ -4,31 +4,54 @@
   };
 })(jQuery);
 
-var clicks = true;
-$(".wrapper__light-switch--lnnbg").click(function (evt) {
-  if (clicks) {
-    switch_mode_from_dark_to_light();
-    clicks = false;
-    localStorage.setItem("mode", "light");
-  } else {
-    switch_mode_from_light_to_dark();
-    clicks = true;
-    localStorage.setItem("mode", "dark");
-  }
-});
-
-// If user has existing mode set
-if (localStorage.getItem("mode")) {
-  let userMode = localStorage.getItem("mode");
-  if (userMode == "light") {
-    $(".wrapper__light-switch--lnnbg").click();
-  }
-}
-
+// Function to switch to light mode
 function switch_mode_from_dark_to_light() {
   $(".is--dark-mode").replaceClass("is--dark-mode", "is--light-mode");
+  localStorage.setItem("mode", "light");
 }
 
+// Function to switch to dark mode
 function switch_mode_from_light_to_dark() {
   $(".is--light-mode").replaceClass("is--light-mode", "is--dark-mode");
+  localStorage.setItem("mode", "dark");
 }
+
+// Set initial mode based on localStorage
+$(document).ready(function () {
+  var clicks = true;
+  
+  // Get references to the tabs for light and dark mode
+  var lightTab = $("[data-theme='light']");
+  var darkTab = $("[data-theme='dark']");
+
+  // If user has an existing mode set
+  let userMode = localStorage.getItem("mode");
+
+  if (userMode === "light") {
+    lightTab.addClass('w--current');
+    darkTab.removeClass('w--current');
+    switch_mode_from_dark_to_light();
+    clicks = true;
+  } else if (userMode === "dark") {
+    darkTab.addClass('w--current');
+    lightTab.removeClass('w--current');
+    switch_mode_from_light_to_dark();
+    clicks = false;
+  }
+
+  // Click handler for light tab
+  lightTab.click(function () {
+    if (!clicks) {
+      switch_mode_from_dark_to_light();
+      clicks = true;
+    }
+  });
+
+  // Click handler for dark tab
+  darkTab.click(function () {
+    if (clicks) {
+      switch_mode_from_light_to_dark();
+      clicks = false;
+    }
+  });
+});
