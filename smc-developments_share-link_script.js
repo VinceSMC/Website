51e31bb9b59d5a23ce50bad5e210a0f3e8054343
Vince <97965361+VinceSMC@users.noreply.github.com>
2024-08-25 19:11:42 +0200

    Update smc-developments_share-link_script.js

--- smc-developments_share-link_script.js
+++ smc-developments_share-link_script.js
@@ -5,57 +5,46 @@ document.addEventListener('DOMContentLoaded', function() {
     shareButtons.forEach(function(button) {
         button.addEventListener('click', function(event) {
             event.preventDefault(); // prevent default action if it's an anchor or a button inside a form
-
-            // Select the overlay
-            const overlay = document.querySelector('[data-modal-overlay]');
-
-            if (overlay) {
-                overlay.style.display = 'flex'; // Ensure the overlay is displayed
-                setTimeout(() => overlay.setAttribute('data-show-modal', 'true'), 10); // Trigger the opacity transition
-            }
+            const overlay = document.querySelector('.changelog__modal-overlay');
+            overlay.style.display = 'flex';
+            setTimeout(() => overlay.classList.add('show-modal'), 10);
 
             // Get the dynamic link from the element with "data-modal-shareable-link" attribute within the closest parent with "data-link-share"
             const dynamicLink = this.closest('[data-link-share]').querySelector('[data-modal-shareable-link]').href;
 
             // Change the text inside the modal to the link
-            const linkText = document.querySelector('[data-modal-link-text]');
-            if (linkText) {
-                linkText.textContent = dynamicLink;
-            }
+            const linkText = document.querySelector('.modal__link-text');
+            linkText.textContent = dynamicLink;
         });
     });
 
-    // Function to hide the modal when overlay or close element is clicked
-    const overlay = document.querySelector('[data-modal-overlay]');
-    if (overlay) {
-        overlay.addEventListener('click', function(e) {
-            if (e.target === this || e.target.closest('[data-modal-close]')) {
-                overlay.removeAttribute('data-show-modal');
-                setTimeout(() => {
-                    overlay.style.display = 'none';
-                }, 300); // Match the duration of the opacity transition
-            }
-        });
-    }
+    // Function to hide the modal when overlay or close SVG is clicked
+    document.querySelector('.changelog__modal-overlay').addEventListener('click', function(e) {
+        if (e.target === this || e.target.closest('.modal__close')) {
+            const overlay = document.querySelector('.changelog__modal-overlay');
+            overlay.classList.remove('show-modal');
+            setTimeout(() => {
+                overlay.style.display = 'none';
+            }, 300);
+        }
+    });
 
     // Code to copy the CMS link
-    const copyButton = document.querySelector('[data-modal-copy-button]');
-    if (copyButton) {
-        copyButton.addEventListener('click', function() {
-            const link = document.querySelector('[data-modal-link-text]').textContent;
-            const tempInput = document.createElement('input');
-            document.body.appendChild(tempInput);
-            tempInput.value = link;
-            tempInput.select();
-            document.execCommand('copy');
-            document.body.removeChild(tempInput);
+    const copyButton = document.querySelector('.modal__copy-button');
+    copyButton.addEventListener('click', function() {
+        const link = document.querySelector('.modal__link-text').textContent;
+        const tempInput = document.createElement('input');
+        document.body.appendChild(tempInput);
+        tempInput.value = link;
+        tempInput.select();
+        document.execCommand('copy');
+        document.body.removeChild(tempInput);
 
-            // Change button text to "Copied!" and revert after 1 second
-            const originalText = copyButton.textContent;
-            copyButton.textContent = 'Copied!';
-            setTimeout(() => {
-                copyButton.textContent = originalText;
-            }, 1000);
-        });
-    }
+        // Change button text to "Copied!" and revert after 1 second
+        const originalText = copyButton.textContent;
+        copyButton.textContent = 'Copied!';
+        setTimeout(() => {
+            copyButton.textContent = originalText;
+        }, 1000);
+    });
 });
